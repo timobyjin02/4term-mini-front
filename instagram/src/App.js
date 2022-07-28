3ec946ba0dc51c56a9a904b7bdb6685e21c0f564
jomi707 <dol7966@gmail.com>
2022-07-28 13:35:24 +0900

    feat(develop): GlobalStyle, Router 설정

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,5 +1,41 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { injectionGlobal } from '@emotion/css';
+
+injectGlobal`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: none;
+    border: none;
+    box-sizing: border-box; 
+  }
+  textarea {
+    resize: none;
+  }
+  button {
+    cursor: pointer;
+    background-color: inherit;
+  } 
+  ul {
+    list-style: none;
+  }
+  li {
+    list-style:none;
+  }
+  a, a:visited, a:link {
+    color: inherit;
+    text-decoration: none;
+  }
+`;
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/"></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
