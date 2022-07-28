8b16c095f8a18b0aea9c135f547f3fdcb04f17e0
jomi707 <dol7966@gmail.com>
2022-07-28 21:12:23 +0900

    refactor(feature/bang/#4): split card in nav, create main

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -4,6 +4,8 @@ import { injectGlobal } from "@emotion/css";
 import Login from "./components/Login";
 import Join from "./components/Join";
 import Navigation from "./components/Navigation";
+import RandomPost from "./components/RandomPost";
+import Main from "./components/Main";
 
 function App() {
   return (
@@ -12,36 +14,11 @@ function App() {
         <Route path="/" element={<Login />}></Route>
         <Route path="/Join" element={<Join />}></Route>
         <Route path="/Navigation" element={<Navigation />}></Route>
+        <Route path="/random" element={<RandomPost />}></Route>
+        <Route path="/main" element={<Main />}></Route>
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
-injectGlobal`
-  * {
-    margin: 0;
-    padding: 0;
-    outline: none;
-    border: none;
-    box-sizing: border-box; 
-  }
-  textarea {
-    resize: none;
-  }
-  button {
-    cursor: pointer;
-    background-color: inherit;
-  } 
-  ul {
-    list-style: none;
-  }
-  li {
-    list-style:none;
-  }
-  a, a:visited, a:link {
-    color: inherit;
-    text-decoration: none;
-  }
-`;
