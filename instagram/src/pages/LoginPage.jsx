fd9c1f0f54a10124404db166698c93f063b690da
ujin <dbwlsxkal95@gmail.com>
2022-08-25 21:38:36 +0900

    feat(feature/ujin/#11)

--- instagram/src/pages/LoginPage.jsx
+++ instagram/src/pages/LoginPage.jsx
@@ -1,47 +1,9 @@
-import React, { useEffect } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import React from "react";
 import { Login } from "../styles/User/LoginPageStyle";
 import LoginBody from "../components/User/LoginBody";
 import LoginFooter from "../components/User/LoginFooter";
-import axios, { setHeader, removeHeader } from "../api/config";
-import { useRecoilState } from "recoil";
-import { userNo } from "../store/user";
 
 function LoginPage() {
-
-  const location = useLocation();
-  const navigate = useNavigate();
-  const [user, setUser] = useRecoilState(userNo);
-
-  async function getProperty(isCurrentRootPath) {
-    try {
-      const response = await axios.get("/user/profile/:userNo");
-      setUser(response.data.userNo);
-      if (isCurrentRootPath) {
-        navigate("/main");
-      }
-    } catch (err) {
-      console.log("로그인 정보가 만료되었습니다");
-      navigate("/");
-      localStorage.removeItem("token");
-      removeHeader();
-    }
-  }
-
-  useEffect(function () {
-    const token = localStorage.getItem("token");
-    const isCurrentRootPath = location.pathname === "/";
-
-    if (!token) {
-      if (!isCurrentRootPath) {
-        navigate("/");
-      }
-    } else {
-      setHeader(token); // token이 있을 때, axios 사용할 때마다 자동으로 헤더에 Authorization 적용될 수 있도록
-      getProperty(isCurrentRootPath);
-    }
-  }, []);
-
   return (
     <Login>
       <LoginBody />
