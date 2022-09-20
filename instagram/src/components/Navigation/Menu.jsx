90f2255d637e00ab5129b4bae5bd3b73ec3d61a4
jomi707 <dol7966@gmail.com>
2022-09-20 14:09:40 +0900

    feat(feature/bang/#41): 네비게이션에서 프로필 출력

--- instagram/src/components/Navigation/Menu.jsx
+++ instagram/src/components/Navigation/Menu.jsx
@@ -1,18 +1,35 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { Wrap, ProfileImage } from "../../styles/Navigation/MenuStyle";
+import { getUserNickname, getUserNo } from "../../utils/getToken";
+import PostModal from "../../pages/PostModal";
 import { ReactComponent as Home } from "../../assets/home.svg";
 import { ReactComponent as Post } from "../../assets/post.svg";
 import { ReactComponent as Notification } from "../../assets/notification.svg";
-import { Wrap, ProfileImage } from "../../styles/Navigation/MenuStyle";
-import PostModal from "../../pages/PostModal";
-import { getUserNickname } from "../../utils/getToken";
 
 function Menu() {
+  const nickname = getUserNickname();
+  // modal for post
   const [showModal, setShowModal] = useState(false);
   const openModal = () => setShowModal(true);
-  const nickname = getUserNickname();
+  // profileImg
+  const [myProfileImg, setMyProfileImg] = useState({
+    isProfileImg: false,
+    profileImg: "",
+  });
 
-  // setShowModal(closed);
+  const getMyProfileImg = async () => {
+    const userNo = getUserNo();
+    const URL = `${process.env.REACT_APP_URL}user/profileImg/42`;
+    const res = await axios.get(URL);
+    setMyProfileImg(res.data);
+    console.log(res);
+  };
+
+  useEffect(() => {
+    getMyProfileImg();
+  }, []);
   return (
     <Wrap>
       <Link to="/main">
@@ -24,7 +41,11 @@ function Menu() {
         <Notification className="icon" />
       </Link>
       <Link to={`/${nickname}`}>
-        <ProfileImage alt="profile image" src="/img/profile.png" />
+        {myProfileImg.isProfileImg ? (
+          <ProfileImage alt="profile image" src={myProfileImg.profileImg} />
+        ) : (
+          <ProfileImage alt="profile image" src="/img/profile.png" />
+        )}
       </Link>
     </Wrap>
   );
