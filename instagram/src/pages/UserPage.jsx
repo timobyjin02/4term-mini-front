40d53ee24ac24617b05fe7eab7b848a8b3eef228
donkey_kong <donghui8266@gmail.com>
2022-09-29 08:23:09 +0900

    feat(feature/donkey/#59): 타인 프로필 구현을 위한 url 파라미터와 토큰 닉네임 비교 검사 추가

--- instagram/src/pages/UserPage.jsx
+++ instagram/src/pages/UserPage.jsx
@@ -5,26 +5,37 @@ import HeaderBaseline from "../components/UserPage/HeaderBaseline";
 import { Container } from "../styles//UserPage/UserPageStyle";
 import { useState, useEffect } from "react";
 import axios from "../api/config";
-import { getUserNo } from "../utils/getToken";
+import { getUserNickname, getUserNo } from "../utils/getToken";
+import { useParams } from "react-router-dom";
 
 function UserPage() {
+  let { nickname } = useParams();
   const userNo = getUserNo();
+  const userNickname = getUserNickname();
+  const [isMyPage, setIsMyPage] = useState(false);
   const [postData, setPostData] = useState({});
 
   useEffect(() => {
+    if (nickname === userNickname) setIsMyPage(true);
     axios.get(`post/profile/${userNo}`).then((res) => {
       setPostData({ ...res.data });
     });
-  }, [userNo]);
+  }, [userNo, nickname, userNickname]);
 
   return (
     <>
-      <Navigation />
-      <Container>
-        <UserHeader userNo={userNo} postData={postData} />
-        <HeaderBaseline />
-        <UserPostings postData={postData} />
-      </Container>
+      {isMyPage ? (
+        <>
+          <Navigation />
+          <Container>
+            <UserHeader userNo={userNo} postData={postData} />
+            <HeaderBaseline />
+            <UserPostings postData={postData} />
+          </Container>
+        </>
+      ) : (
+        <div>남의 페이지다</div>
+      )}
     </>
   );
 }
