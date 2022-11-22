b2bf710e8ecb30bec559b871fab87b55b71f2ec9
donkey_kong <donghui8266@gmail.com>
2022-11-22 15:17:39 +0900

    css(feature/donkey/#79): index.css 파일의 html, body css 수정

--- instagram/src/styles/UserPage/UserPageStyle.jsx
+++ instagram/src/styles/UserPage/UserPageStyle.jsx
@@ -1,11 +1,12 @@
 import styled from "@emotion/styled";
 
 export const Container = styled.div`
+  position: absolute;
   width: 100%;
-  height: 100%;
-
+  height: auto;
+  inset: 60px 0px 0px;
+  background-color: #fafafa;
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color: #fafafa;
 `;
